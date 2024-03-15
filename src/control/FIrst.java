1e9e83fefdf8602e118222161eea0f516c085192
zeroz <zeroz@192.168.11.5>
2024-03-16 02:03:00 +0900

    second commit

---
+++ src/control/FIrst.java
@@ -0,0 +1,17 @@
+package control;
+
+public class FIrst {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		for (int i = 1; i < 10; i++) { //iは1から10まで繰り返し
+			for (int j = 1; j < 10; j++) {//jも1から10まで繰り返し
+				System.out.print(i * j);//掛け算の結果を出力
+				System.out.print(" ");//空白を出力
+			}
+			System.out.println("");//改行を出力
+		}
+	}
+
+}
