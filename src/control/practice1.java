06f9a338dd7b9cfc083cb4e873994016865f0442
zeroz <zeroz@192.168.11.5>
2024-03-23 14:24:31 +0900

    練習問題まで

---
+++ src/control/practice1.java
@@ -0,0 +1,13 @@
+package control;
+
+public class practice1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		for (int i = 0; i < 3; i++) {
+			System.out.print("現在" + (i + 1) + "周目→" );
+		}
+	}
+
+}
