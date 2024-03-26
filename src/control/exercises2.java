dab5e37af2ce7b9ce77637838e7c31c3cd3f7749
zeroz <zeroz@192.168.11.5>
2024-03-26 18:46:30 +0900

    練習問題3-4

---
+++ src/control/exercises2.java
@@ -0,0 +1,18 @@
+package control;
+
+public class exercises2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		boolean tenki = false;
+		if (tenki == true) {
+			System.out.println("洗濯をします");
+			System.out.println("散歩に行きます");
+		} else {
+			System.out.println("DVDを見ます");
+			System.out.println("寝ます");
+		}
+	}
+
+}
