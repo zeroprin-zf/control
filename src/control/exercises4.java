dbe618194bf5541a825f6458a419143757d2aeef
zeroz <zeroz@192.168.11.5>
2024-03-27 02:49:59 +0900

    練習問題3-6

---
+++ src/control/exercises4.java
@@ -0,0 +1,23 @@
+package control;
+
+public class exercises4 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		System.out.print("【数あてゲーム】");
+		int ans = new java.util.Random().nextInt(10);
+		for (int i = 0; i < 5; i++) {
+			System.out.println("0～9の数字を入力してください");
+			int num = new java.util.Scanner(System.in) .nextInt();
+			if (ans == num) {
+				System.out.println("アタリ!");
+				break;
+			}else {
+				System.out.println("違います");
+			}
+		}
+		System.out.println("ゲームを終了します");
+	}
+
+}
