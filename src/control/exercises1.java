8e6131916a7ba66aa0d163fc615349afa6ea4140
zeroz <zeroz@192.168.11.5>
2024-03-26 18:36:38 +0900

    練習問題3-3

--- src/control/exercises1.java
+++ src/control/exercises1.java
@@ -5,7 +5,18 @@ public class exercises1 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
-		
+		int isHungry = 1;
+		String food = "おにぎり";
+		System.out.println("こんにちは");
+		if (isHungry == 0) {
+			System.out.println("お腹がいっぱいです");
+		} else {
+			System.out.println("腹ペコです");
+		}
+		if (isHungry == 1) {
+			System.out.println(food + "をいただきます");
+		}
+		System.out.println("ごちそうさまでした");
 	}
 
 }
