08a0841700c0a8a58b286d5e2828313415dfc7dd
zeroz <zeroz@192.168.11.5>
2024-03-27 16:27:44 +0900

    配列の初期

---
+++ src/control/array1.java
@@ -0,0 +1,13 @@
+package control;
+
+public class array1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		int[] scores = new int[5];
+		int num = scores.length;
+		System.out.println("要素の数: " + num);
+	}
+
+}
